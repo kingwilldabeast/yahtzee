59ceb447bbfa0868d36977b889a18bbe04a50007
kingwilldabeast <willmur@live.com>
2024-05-09 17:27:18 -0500

    added files and pseudocode

---
+++ script.js
@@ -0,0 +1,32 @@
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+// roll all unlocked dice by generating 1-6 random
+
+// calculate scores for potential patterns based on dice values
+
+// lock or unlock an individual die
+
+// select single pattern and update score
+
+// reset dice at beginning of new turn
+
+// game ends after all 13 patterns have been selected
+
+// reset all scores and dice at end of game
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+die1.addEventListener('click', myFunction)
